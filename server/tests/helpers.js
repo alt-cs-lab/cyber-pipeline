07cde74ca503a1e80634743faba0c8e952137b5f
JKenslow <jkenslow@ksu.edu>
2024-09-09 10:27:08 -0500

    added helpers js for admin

---
+++ server/tests/helpers.js
@@ -0,0 +1,27 @@
+//Require the dev-dependencies
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+chai.use(chaiHttp)
+require('chai').should()
+
+//Require app dependencies
+var app = require('../app')
+
+const loginAsAdmin = function (done) {
+  var agent = chai.request.agent(app)
+  agent.get('/auth/login?eid=test-admin').end(() => {
+    agent.get('/auth/token').end((err, res) => {
+      this.token = res.body.token
+      if (!this.tokens) this.tokens = {}
+      this.tokens['admin'] = res.body.token
+      res.should.have.status(200)
+      agent.close()
+      done()
+    })
+  })
+}
+
+module.exports = {
+    loginAsAdmin: loginAsAdmin,
+  }
+  
