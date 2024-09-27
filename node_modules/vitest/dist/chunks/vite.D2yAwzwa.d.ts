8a1124ed0493f99638f8130b20ceb98a0f8a0746
Devon-Hill7 <123010104+Devon-Hill7@users.noreply.github.com>
2024-09-27 16:16:48 +0000

    Switched Sever to Vitest

---
+++ node_modules/vitest/dist/chunks/vite.D2yAwzwa.d.ts
@@ -0,0 +1,11 @@
+import { I as InlineConfig } from './reporters.WnPwkmgA.js';
+
+type VitestInlineConfig = InlineConfig;
+declare module 'vite' {
+    interface UserConfig {
+        /**
+         * Options for Vitest
+         */
+        test?: VitestInlineConfig;
+    }
+}
